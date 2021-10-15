e24b9049976b4de976f12351e3f3e32854224b78
McHenry <lukemchenry311@gmail.com>
2021-10-15 10:03:36 -0600

    add service worker and manifest

---
+++ public/service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = "my-site-cahce-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/favicon.ico",
+  "/manifest.json",
+  "/assets/css/style.css",
+  "/assets/js/loadImages.js",
+  "/assets/images/icons/icon-72x72.png",
+  "/assets/images/icons/icon-96x96.png",
+  "/assets/images/icons/icon-128x128.png",
+  "/assets/images/icons/icon-144x144.png",
+  "/assets/images/1.jpg",
+  "/assets/images/2.jpg",
+  "/assets/images/3.jpg",
+  "/assets/images/4.jpg",
+  "/assets/images/5.jpg",
+  "/assets/images/6.jpg",
+  "/assets/images/7.jpg",
+  "/assets/images/8.jpg",
+  "/assets/images/9.jpg",
+];
+
+self.addEventListener("install", function (evt) {
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("files cached!");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("removing past cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
